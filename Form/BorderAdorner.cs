e6e4f17c7e9699bc7db52c2f63587879032783d5
omaralian98 <omaralian9870@gmail.com>
2024-03-14 11:19:29 +0300

    Fixed Edges

---
+++ Form/BorderAdorner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace Form;
+
+public class BorderAdorner : Adorner
+{
+    // Be sure to call the base class constructor.
+    public BorderAdorner(UIElement adornedElement) : base(adornedElement)
+    {
+    }
+
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+        var adornedElementRect = new Rect(this.AdornedElement.DesiredSize);
+        var pen = new Pen(Brushes.DarkBlue, 1.5);
+
+        // Inflate the rectangle to create space for the ellipse
+        adornedElementRect.Inflate(5, 5);
+
+        // Calculate the center of the ellipse
+        var centerX = adornedElementRect.Left + adornedElementRect.Width / 2;
+        var centerY = adornedElementRect.Top + adornedElementRect.Height / 2;
+
+        // Calculate the radius of the ellipse
+        var radiusX = adornedElementRect.Width / 2;
+        var radiusY = adornedElementRect.Height / 2;
+
+        // Draw the ellipse
+        drawingContext.DrawEllipse(null, pen, new Point(centerX, centerY), radiusX, radiusY);
+    }
+
+}
--- Form/Main/View/GraphView.xaml.cs
+++ Form/Main/View/GraphView.xaml.cs
@@ -1,27 +1,14 @@
 ﻿using Search_Algorithms;
 using Search_Algorithms.Algorithms;
 using Search_Algorithms.Games.Graph;
-using Search_Algorithms.Games.Shortest_Path;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Xml.Linq;
 
 namespace Form.Main.View;
 
@@ -50,7 +37,7 @@ public partial class GraphView : UserControl
         InitializeComponent();
     }
 
-    /// <summary>
+    /// <summary>N
     /// The Event of draging elements from the toolbox
     /// </summary>
     /// <param name="sender"></param>
@@ -121,6 +108,7 @@ public partial class GraphView : UserControl
             Width = 80,
             Height = 80,
         };
+
         SetNodeBackground(newNode, CellColour.Default);
         ApplyNodeStyleToButton(newNode);
         newNode.PreviewMouseDown += Node_PreviewMouseDown;
@@ -187,14 +175,16 @@ public partial class GraphView : UserControl
         }
     }
 
-    private static void AddAdorner(Button button)
+    private static void AddAdorner(Button? button, Adorner? adorner = null)
     {
+        if (button is null) return;
         AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(button);
-        adornerLayer?.Add(new ButtonCustomAdorner(button));
+        adornerLayer?.Add(adorner ?? new ButtonCustomAdorner(button));
     }
 
-    private static void RemoveAdorner(Button button)
+    private static void RemoveAdorner(Button? button)
     {
+        if (button is null) return;
         AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(button);
         Adorner[]? adorners = adornerLayer?.GetAdorners(button);
 
@@ -202,11 +192,8 @@ public partial class GraphView : UserControl
         {
             foreach (Adorner adorner in adorners)
             {
-                if (adorner is ButtonCustomAdorner)
-                {
-                    adornerLayer?.Remove(adorner);
-                    break;
-                }
+                adornerLayer?.Remove(adorner);
+                break;
             }
         }
     }
@@ -222,16 +209,36 @@ public partial class GraphView : UserControl
     private void Node_PreviewMouseDown(object sender, MouseButtonEventArgs e)
     {
         // If AddLine is Clicked then we draw a line and save the Id of this node to be the source of this line
-        if (IsAddLineClicked)
-        {
-            sourceNodeId = Convert.ToInt32(((Button)sender).Tag);
-            Graph_MouseDown(sender, e);
-            return;
-        } 
-        else if (sender is Button button)
+        if (sender is Button button)
         {
             var nodeId = Convert.ToInt32(button.Tag);
-            if (e.RightButton == MouseButtonState.Pressed)
+
+            if (IsAddLineClicked)
+            {
+                if (sourceNodeId == -1)
+                {
+                    sourceNodeId = nodeId;
+                    AddAdorner(button, new BorderAdorner(button));
+                }
+                else
+                {
+                    var destinationNodeId = Convert.ToInt32(((Button)sender).Tag);
+                    var sourceNode = (Button)Graph.Children[NodesIndex.GetValueOrDefault(sourceNodeId)];
+                    if (sourceNodeId == destinationNodeId)
+                    {
+                        ConnectNodeToItSelf(sourceNode);
+                    }
+                    else
+                    {
+                        var destinationNode = (Button)Graph.Children[NodesIndex.GetValueOrDefault(destinationNodeId)];
+                        ConnectTwoNodes(sourceNode, destinationNode);
+                    }
+                    RemoveAdorner(sourceNode);
+                    sourceNodeId = -1;
+                }
+            }
+
+            else if (e.RightButton == MouseButtonState.Pressed)
             {
                 // If Start Node is not set
                 if (StartNodeId == -1 && nodeId != TargetNodeId)
@@ -276,21 +283,19 @@ public partial class GraphView : UserControl
 
     private void Node_PreviewMouseMove(object sender, MouseEventArgs e)
     {
-        if (isDragging && sender is Button button && e.LeftButton == MouseButtonState.Pressed)
+        if (isDragging && sender is Button grid && e.LeftButton == MouseButtonState.Pressed)
         {
             Point currentPosition = e.GetPosition(Graph);
             double deltaX = currentPosition.X - originalPosition.X;
             double deltaY = currentPosition.Y - originalPosition.Y;
 
-            double newX = Canvas.GetLeft(button) + deltaX;
-            double newY = Canvas.GetTop(button) + deltaY;
+            double newX = Canvas.GetLeft(grid) + deltaX;
+            double newY = Canvas.GetTop(grid) + deltaY;
 
-            newX = Math.Max(0, Math.Min(Graph.ActualWidth - button.ActualWidth, newX));
-            newY = Math.Max(0, Math.Min(Graph.ActualHeight - button.ActualHeight, newY));
-
-            Canvas.SetLeft(button, newX);
-            Canvas.SetTop(button, newY);
-
+            newX = Math.Max(0, Math.Min(Graph.ActualWidth - grid.ActualWidth, newX));
+            newY = Math.Max(0, Math.Min(Graph.ActualHeight - grid.ActualHeight, newY));
+            Canvas.SetLeft(grid, newX);
+            Canvas.SetTop(grid, newY);
             originalPosition = currentPosition;
         }
     }
@@ -305,6 +310,7 @@ public partial class GraphView : UserControl
     }
 
     public bool _IsAddLineClicked = false;
+    private string AddLineMode;
     public bool IsAddLineClicked
     {
         get
@@ -314,92 +320,10 @@ public partial class GraphView : UserControl
         set
         {
             _IsAddLineClicked = value;
-            CheckBox_Checked();
-        }
-    }
-    private Point startPoint;
-    private Line currentLine;
-
-    private void Graph_MouseDown(object sender, MouseButtonEventArgs e)
-    {
-        if (IsAddLineClicked)
-        {
-            Point position = e.GetPosition(Graph);
-
-            if (double.IsNaN(startPoint.X))
-            {
-                startPoint = position;
-            }
-            else
-            {
-                CheckBox_Checked();
-            }
-            if (sender is not Button)
-            {
-                Point hitPoint = e.GetPosition(Graph);
-                VisualTreeHelper.HitTest(Graph, null, new HitTestResultCallback((result) =>
-                {
-                    if (result.VisualHit is Ellipse ellipse)
-                    {
-                        var destinationNodeId = Convert.ToInt32(ellipse.Tag);
-                        if (sourceNodeId != -1)
-                        {
-                            graph.AddEdge(sourceNodeId, destinationNodeId);
-                        }
-                        return HitTestResultBehavior.Stop;
-                    }
-                    return HitTestResultBehavior.Continue;
-                }), new PointHitTestParameters(hitPoint));
-
-            }
-        }
-    }
-    private void Graph_MouseMove(object sender, MouseEventArgs e)
-    {
-        if (IsAddLineClicked)
-        {
-            // Get the mouse position relative to the canvas
-            Point position = e.GetPosition(Graph);
-
-            // If startPoint is set, update the line's endpoint
-            if (!double.IsNaN(startPoint.X))
-            {
-                currentLine.X1 = startPoint.X;
-                currentLine.Y1 = startPoint.Y;
-                currentLine.X2 = position.X;
-                currentLine.Y2 = position.Y;
-            }
+            RemoveAdorner(adornerBtn);
         }
     }
 
-    private void CheckBox_Checked()
-    {
-        if (IsAddLineClicked)
-        {
-            startPoint = new Point(double.NaN, double.NaN);
-            currentLine = new Line
-            {
-                Stroke = Brushes.Black
-            };
-            Graph.Children.Add(currentLine);
-        }
-
-    }
-
-    private void TriggerHolders()
-    {
-        if (IsAddLineClicked)
-        {
-            foreach (var nodeIndex in NodesIndex)
-            {
-
-            }
-        }
-        else
-        {
-
-        }
-    }
 
     private async void Search_Click(object sender, RoutedEventArgs e)
     {
@@ -417,10 +341,10 @@ public partial class GraphView : UserControl
             switch (Algorithms.SelectedIndex)
             {
                 case 0:
-                    //res = await AStarSearch.FindPath(grid.Start, Search_Algorithms.Games.Shortest_Path.Grid.ManhattanDistance, delay: Convert.ToInt32(Delay * 1000), token: cancellationTokenSource.Token);
+                    //res = await AStarSearch.FindPath(grid.Start, Search_Algorithms.Games.Shortest_Path.Button.ManhattanDistance, delay: Convert.ToInt32(Delay * 1000), token: cancellationTokenSource.Token);
                     break;
                 case 1:
-                    //res = await Greedy_Best_First_Search.FindPath(grid.Start, Search_Algorithms.Games.Shortest_Path.Grid.ManhattanDistance, delay: Convert.ToInt32(Delay * 1000), token: cancellationTokenSource.Token);
+                    //res = await Greedy_Best_First_Search.FindPath(grid.Start, Search_Algorithms.Games.Shortest_Path.Button.ManhattanDistance, delay: Convert.ToInt32(Delay * 1000), token: cancellationTokenSource.Token);
                     break;
                 case 2:
                     res = await Breadth_First_Search.FindPath(graph.StartNode, delay: Convert.ToInt32(Delay * 1000));
@@ -429,7 +353,7 @@ public partial class GraphView : UserControl
                     res = await Depth_First_Search.FindPath(graph.StartNode, delay: Convert.ToInt32(Delay * 1000));
                     break;
                 case 4:
-                    //res = await Hill_Climbing.FindPath(graph.StartNode, Search_Algorithms.Games.Shortest_Path.Grid.ManhattanDistance, delay: Convert.ToInt32(Delay * 1000), token: cancellationTokenSource.Token);
+                    //res = await Hill_Climbing.FindPath(graph.StartNode, Search_Algorithms.Games.Shortest_Path.Button.ManhattanDistance, delay: Convert.ToInt32(Delay * 1000), token: cancellationTokenSource.Token);
                     break;
             }
             MessageBox.Show(res.Steps.Count.ToString());
@@ -499,5 +423,91 @@ public partial class GraphView : UserControl
             default:
                 break;
         }
+    }
+
+    private void ConnectTwoNodes(Button first, Button second)
+    {
+        static double GetAngle(double x1, double y1, double x2, double y2) => Math.Atan2(y2 - y1, x2 - x1);
+        void DrawArrow(Point startPoint, Point endPoint, SolidColorBrush color)
+        {
+            var line = new Line
+            {
+                X1 = startPoint.X,
+                Y1 = startPoint.Y,
+                X2 = endPoint.X,
+                Y2 = endPoint.Y,
+                Stroke = color
+            };
+            Graph.Children.Add(line);
+
+            // Calculate angle of the line
+            var angle = Math.Atan2(endPoint.Y - startPoint.Y, endPoint.X - startPoint.X);
+
+            // Draw arrowhead
+            var arrowAngle = Math.PI / 6; // 30 degrees
+            var arrowLength = 10;
+            var arrowPoint1 = new Point(
+                endPoint.X - arrowLength * Math.Cos(angle - arrowAngle),
+                endPoint.Y - arrowLength * Math.Sin(angle - arrowAngle)
+            );
+            var arrowPoint2 = new Point(
+                endPoint.X - arrowLength * Math.Cos(angle + arrowAngle),
+                endPoint.Y - arrowLength * Math.Sin(angle + arrowAngle)
+            );
+            var arrowHead = new Polygon
+            {
+                Stroke = color,
+                Fill = color,
+                Points = [endPoint, arrowPoint1, arrowPoint2]
+            };
+            Graph.Children.Add(arrowHead);
+        }
+
+        var radius1 = first.ActualWidth / 2;
+        var radius2 = second.ActualWidth / 2;
+
+        var x1 = Canvas.GetLeft(first) + radius1;
+        var y1 = Canvas.GetTop(first) + radius1;
+        var x2 = Canvas.GetLeft(second) + radius2;
+        var y2 = Canvas.GetTop(second) + radius2;
+
+        var startPoint = new Point(x1 + radius1 * Math.Cos(GetAngle(x1, y1, x2, y2)), y1 + radius1 * Math.Sin(GetAngle(x1, y1, x2, y2)));
+        var endPoint = new Point(x2 + radius2 * Math.Cos(GetAngle(x2, y2, x1, y1)), y2 + radius2 * Math.Sin(GetAngle(x2, y2, x1, y1)));
+
+        var destinationNodeId = Convert.ToInt32(second.Tag);
+
+
+
+        switch (AddLineMode)
+        {
+            case "UL":
+                DrawArrow(startPoint, endPoint, Brushes.Black);
+                DrawArrow(endPoint, startPoint, Brushes.Black);
+                graph.AddEdge(sourceNodeId, destinationNodeId);
+                graph.AddEdge(destinationNodeId, sourceNodeId);
+                break;
+            case "DL":
+                DrawArrow(startPoint, endPoint, Brushes.Black);
+                graph.AddEdge(sourceNodeId, destinationNodeId);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ConnectNodeToItSelf(Button node)
+    {
+        var nodeId = Convert.ToInt32(node.Tag);
+        graph.AddEdge(nodeId, nodeId);
+    }
+
+
+
+
+
+
+    private void AddLine_Checked(object sender, RoutedEventArgs e)
+    {
+        AddLineMode = ((ToggleButton)sender).Tag.ToString() ?? "";
     }
 }
